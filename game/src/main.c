639c1cc1fc531609711c732e0293b3334990450c
franciszekgotkowski <paprykojad@gmail.com>
2025-12-25 12:57:51 +0100

    wypisuje argv z gry

--- game/src/main.c
+++ game/src/main.c
@@ -1,5 +1,10 @@
 #include "../../engine/headers/internal/print.h"
+#include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     print(67);
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 }
