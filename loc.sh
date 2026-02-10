fac483096a27bed7356aa59f7fe622f2297271b4
franciszekgotkowski <paprykojad@gmail.com>
2026-02-10 17:25:03 +0100

    added loc.sh file which counts lines of code i wrote

---
+++ loc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the directories to search
+TARGET_DIRS=(
+  "engine/src"
+  "engine/headers/engine"
+  "game/src"
+  "game/headers/game"
+)
+
+# Use find to locate .c and .h files and pass them to wc -l
+find "${TARGET_DIRS[@]}" -type f \( -name "*.c" -o -name "*.h" \) -exec wc -l {} +
